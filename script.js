6847b0e9dc04d98fa4dbcfd8e2aa76efbbfa92e8
rajeebrath <128907216+rajeebrath@users.noreply.github.com>
2024-05-28 23:08:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,11 +56,11 @@ function processCommand(command) {
     switch (command) {
         case "HELP":
             result +=
-                "<br>HELP &nbsp&nbsp&nbsp&nbsp&nbsp Displays a list of available commands.<br>\
-                TECH &nbsp&nbsp&nbsp&nbsp&nbsp See the tech tools.<br>\
-                NEWS &nbsp&nbsp&nbsp&nbsp&nbsp Watch out Global News.<br>\
-                JOKES &nbsp&nbsp&nbsp&nbsp Tell me a joke.<br>\
-                WEATHER &nbsp&nbsp Get the weather details.<br>\
+                "<br>HELP &nbsp&nbsp&nbsp&nbsp List all available commands.<br>\
+                TECH &nbsp&nbsp&nbsp&nbsp See the tech tools.<br>\
+                NEWS &nbsp&nbsp&nbsp&nbsp Watch out Global News.<br>\
+                JOKES &nbsp&nbsp&nbsp Tell me a joke.<br>\
+                WEATHER &nbsp Get the weather details.<br>\
                 --------------------------------<br>\
                 USER &nbsp&nbsp&nbsp Login to the console.<br>\
                 EXIT &nbsp&nbsp&nbsp Clear the console.<br>\
@@ -117,15 +117,6 @@ function processCommand(command) {
             &nbsp;&nbsp;-  <a href='https://www.android.com/intl/en_in/'>Android</a><br>"; // Replace '#' with the link you want
             break;
 
-        case "PROJECTS":
-            result +=
-                "<br>	Creation of various resume models.<br>\
-                Minigames in JavaScript<br>\
-                School projects:<br>\
-                &nbsp&nbspPicross in Java<br>\
-                &nbsp&nbspWargame in Java<br>";
-            break;
-
         case "EXIT":
             void window.close();
             break;
@@ -139,16 +130,15 @@ function processCommand(command) {
             break;
 //user cases added
         case "CHUCHU":
-            result +=
-                "<br>	HELP &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Displays a list of available commands.<br>\
-                CONTACT &nbsp&nbsp&nbsp&nbsp&nbsp Displays contact information.<br>\
-                SKILLS &nbsp&nbsp Displays skills.<br>\
-                DEGREES &nbsp&nbsp Displays degrees.<br>\
-                PERSONALINFO &nbsp&nbsp Displays personal information.<br>\
-                EXPERIENCE &nbsp&nbsp Displays experiences.<br>\
-                CLEAR &nbsp&nbsp&nbsp&nbsp&nbsp Clears the console.<br>\
-                LINKS &nbsp&nbsp&nbsp&nbsp&nbsp Displays social links.<br>\
-                PROJECTS &nbsp&nbsp&nbsp Displays a list of projects the site creator has worked on.<br>";
+            result +=  "<br>• Reply the code:<br>\
+            &nbsp;&nbsp;-  Code<1818>: Who is he?<br>\
+            &nbsp;&nbsp;-  Code<0724>: When did you know each other?<br>\
+            &nbsp;&nbsp;-  Code<0602>: How it all Started?<br>\
+            &nbsp;&nbsp;-  Code<2303>: When he proposed?<br>\
+            &nbsp;&nbsp;-  Code<3003>: Fist meet?<br>\
+            &nbsp;&nbsp;-  Code<2504>: Fist kiss?<br>\
+            &nbsp;&nbsp;-  Code<1705>: Most memorable?<br>\
+            &nbsp;&nbsp;-  <a href='https://wa.me/918249079963?text=Helllo%20Babes%20'>Message him</a><br>"; // Replace '#' with the link you want
             break;
 
             case "GUGULU":
@@ -163,6 +153,52 @@ function processCommand(command) {
                     LINKS &nbsp&nbsp&nbsp&nbsp&nbsp Displays social links.<br>\
                     PROJECTS &nbsp&nbsp&nbsp Displays a list of projects the site creator has worked on.<br>";
                 break;
+
+                case "1818":
+                    result +=
+                        "<br>• WHO IS HE?:<br>\
+                        &nbsp;&nbsp;-  <span class='highlight'>Rajeeb Rath</span><br>";
+                break;
+
+                case "0724":
+                    result +=
+                        "<br>• WHEN DID YOU KNOW EACH OTHER?:<br>\
+                        &nbsp;&nbsp;-  <span class='highlight'>7 JAN 24 at 23:56</span><br>";
+                break;
+
+                case "0602":
+                    result +=
+                        "<br>• HOW IT ALL STARTED?:<br>\
+                        &nbsp;&nbsp;-  <span class='highlight'>Started as reply in Facebook story on 7th Jan 24 and migrated to instagram and Snapchat to Whatsapp to Love.</span><br>";
+                break;
+
+                case "2303":
+                    result +=
+                        "<br>• WHEN HE PROPOSED?:<br>\
+                        &nbsp;&nbsp;-  <span class='highlight'>In the middle of the night he proposed and she said yes, 23 MAR 24 at 1:21</span><br>";
+                break;
+
+                case "3003":
+                    result +=
+                        "<br>• FIRST MEET?:<br>\
+                        &nbsp;&nbsp;-  <span class='highlight'>Udala Honda Showroom at 17:40</span><br>";
+                break;
+
+                case "2504":
+                    result +=
+                        "<br>• FIRST KISS?:<br>\
+                        &nbsp;&nbsp;-  <span class='highlight'>Second date and first kiss on 25 APR 24</span><br>";
+                break;
+
+                case "1705":
+                    result +=
+                        "<br>• MOST MEMORABLE?:<br>\
+                        &nbsp;&nbsp;-  <span class='highlight'>Fouth date, Spend many times together and get a new experience on 17 MAY 24</span><br>";
+                break;
+
+
+
+
 
         default:
             result = "<br>" + command + " is not recognized as a command.<br>";
